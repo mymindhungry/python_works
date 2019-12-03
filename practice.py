c0cb277801109ea5d8646dcf7138400f28aea166
Firoj <mymindhungry@gmail.com>
2019-12-03 16:58:16 +0545

    updated

--- practice.py
+++ practice.py
@@ -192,10 +192,18 @@ word = 'python'
 
 # ==========4.4. break and continue Statements, and else Clauses on Loops¶========
 
-for n in range(2, 10):
-    for x in range(2, n):
-        if n % x == 0:
-            print(n, 'equal', x, '*', n // x)
-            break
-    else:
-        print(n, 'is a prime number')
+# for n in range(2, 10):
+#     for x in range(2, n):
+#         if n % x == 0:
+#             print(n, 'equal', x, '*', n // x)
+#             break
+#     else:
+#         print(n, 'is a prime number')
+
+# =======================================
+
+for num in range(2, 10):
+    if num % 2 == 0:
+        print("found an even number", num)
+        continue
+    print("found a number", num)
